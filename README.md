7b4a88356e4efb9821e4a1815e07aa58bd12a925
jnines <jason.mullenberg@gmail.com>
2022-11-17 16:02:52 -0600

    Rewrite to API-ish

---
+++ README.md
@@ -0,0 +1,34 @@
+## MyTab
+
+A very (very) quickly written php script to return server/service status in JSON format. Used for [MyTab](https://github.com/jnines/MyTab). If for whatever reason you decide to use this, know you will likely have to play with [CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS).
+
+#### Configuration and return
+
+Config is at the top of script.
+
+```
+$server_name = "https://ash.lan"; # Address to use if not specified below;  Only one instance.
+
+$servers[] = array(
+    "name" => "jellyFin", # The name you'd like to use
+    "address" => "", # URL or IP address
+    "port" => "", # Port if needed
+    "url" => "/jellyfin/web/index.html", # Path after base address above
+    "fa_icon" => "fas fa-circle-play" # FontAwesome Icon code to use
+);
+```
+
+Returns
+
+```
+[
+  {
+    "name": "jellyFin",
+    "address": "https://ash.lan",
+    "port": "",
+    "fa_icon": "fas fa-circle-play",
+    "url": "/jellyfin/web/index.html",
+    "status": "online"
+  },
+]
+```
---
+++ services.php
@@ -0,0 +1,97 @@
+<?php
+header("Content-Type: application/json");
+/* Configure Start */
+$server_name = "https://ash.lan";
+/*
+$servers[] = array(
+    "name" => "NAME to give server",
+    "address" => "IP/ADDRESS if different from $server_name",
+    "port" => "PORT"
+    );
+*/
+/* Icons from fontawesome.com */
+$servers[] = array(
+    "name" => "jellyFin",
+    "address" => "",
+    "port" => "",
+    "url" => "/jellyfin/web/index.html",
+    "fa_icon" => "fas fa-circle-play"
+);
+$servers[] = array(
+    "name" => "plex",
+    "address" => "",
+    "port" => "",
+    "url" => "/web",
+    "fa_icon" => "fas fa-circle-play"
+);
+$servers[] = array(
+    "name" => "sonarr",
+    "address" => "",
+    "port" => "",
+    "url" => "/sonarr/calendar",
+    "fa_icon" => "fas fa-tv"
+);
+$servers[] = array(
+    "name" => "radarr",
+    "address" => "",
+    "port" => "",
+    "url" => "/radarr",
+    "fa_icon" => "fas fa-film"
+);
+$servers[] = array(
+    "name" => "lidarr",
+    "address" => "",
+    "port" => "",
+    "url" => "/lidarr",
+    "fa_icon" => "fas fa-music"
+);
+$servers[] = array(
+    "name" => "prowlarr",
+    "address" => "",
+    "port" => "",
+    "url" => "/prowlarr",
+    "fa_icon" => "fas fa-list-ul"
+);
+$servers[] = array(
+    "name" => "ombi",
+    "address" => "",
+    "port" => "",
+    "url" => "/ombi/requests-list",
+    "fa_icon" => "fas fa-magnifying-glass"
+);
+$servers[] = array(
+    "name" => "transmission",
+    "address" => "",
+    "port" => "",
+    "url" => "/transmission/web/",
+    "fa_icon" => "fas fa-cloud-arrow-down"
+);
+
+/* Config End */
+$server_up = "";
+foreach ($servers as $server) {
+    if ($server['address'] == "") {
+        $server['address'] = $server_name;
+    }
+    $curl = curl_init($server['address']);
+    @curl_setopt($curl, CURLOPT_PORT, $server['port']);
+    @curl_setopt($curl, CURLOPT_NOBODY, true);
+    @curl_setopt($curl, CURLOPT_FAILONERROR, true);
+    @curl_setopt($curl, CURLOPT_HEADER, false);
+    @curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    @curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+    $service_name = $server['name'];
+    $response = curl_exec($curl);
+    $http_code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+
+    if (!in_array($http_code, ['200', '301', '302', '401'], false)) {
+        $server_up = 'offline';
+    } else {
+        $server_up = 'online';
+    }
+    $set_json[] = ["name" => "$server[name]", "address" => "$server[address]", "port" => "$server[port]", "fa_icon" => "$server[fa_icon]", "url" => "$server[url]", "status" => "$server_up"];
+    curl_close($curl);
+}
+$json = json_encode($set_json);
+echo $json;
